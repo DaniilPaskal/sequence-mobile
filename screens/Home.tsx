519381b80c212778bd839b921f46a293895c798c
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-04 23:54:09 -0400

    Added button to navigate to game screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import type { RootStackParamList } from "../navigation/types";
 
-const Home = () => {
+type HomeProps = NativeStackScreenProps<RootStackParamList>;
+
+const Home = ({ navigation }: HomeProps) => {
     return (
         <View style={styles.container}>
-            <Text></Text>
+            <Pressable style={styles.button} onPress={() => navigation.navigate('GameBoard')}>
+                <Text>Start Game</Text>
+            </Pressable>
         </View>
     );
 }
@@ -15,6 +21,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    button: {
+
+    }
 });
 
 export default Home;
